9ecd18cb0c0d0212062848c3cc624c5369b2a724
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-08-05 16:12:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  document.querySelector(selector);
 
+}
+function nestedTarget(){
+  document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+      const firstList = rankedLists[0]
+      const secondList = rankedLists[1]
+
+      let children = firstList.children
+      let start = 1
+      for (let i = 0, i<children.length; i++) {
+        parseInt(children[i].innerHTML)).to.equal(start + i + 3)
+      }
+
+}
+
+function deepestChild(){
+  document.querySelector('#grand-node div div div div')
+}
